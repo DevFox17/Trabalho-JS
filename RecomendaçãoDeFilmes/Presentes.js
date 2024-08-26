ca2df80655f73b9e497ab5a0d3299970b4e58ccd
jandavi <rodriguesjandavi193@gmail.com>
2024-08-26 14:27:42 -0300

    Empacotameto de Presentes

--- RecomendaçãoDeFilmes/Presentes.js
+++ RecomendaçãoDeFilmes/Presentes.js
@@ -1,5 +1,32 @@
 function empacotarpresente(caixas, presentes){
     caixas.sort((x, y)=> x - y)
     presentes.sort((x, y) => x - y)
-    
+
+    let resultado = []
+    let caixa = 0
+    let presente = 0
+
+    while (presente < presentes.length && caixa < caixas.length){
+        if (caixas[caixa] >= presentes[presente]){
+            resultado.push([caixas[caixa], presentes[presente]])
+            caixa++
+            presente++
+        }
+        else{
+            caixa++
+        }
+    }
+
+    if(presente === presentes.length){
+        return resultado
+    }
+    else{
+        return "Não há caixas o suficiente"
+    }
+
 }
+const empacotando = [
+    caixas = [3, 8, 5, 2],
+    presentes = [3, 2, 5, 8],
+]
+console.log(empacotarpresente(caixas, presentes))
